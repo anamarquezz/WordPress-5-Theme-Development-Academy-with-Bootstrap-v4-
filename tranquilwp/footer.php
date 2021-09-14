2d272e3eb773ebbdfe4084badb689da985a628f4
AnaMarquez <anamarquez.edu@hotmail.com>
2021-09-13 22:54:07 -0700

    Making the header and footer dynamic

--- tranquilwp/footer.php
+++ tranquilwp/footer.php
@@ -1,4 +1,8 @@
-     <!-- FOOTER SECTION-->
+<?php 
+/*
+* The template for displaying the footer
+*/?>
+<!-- FOOTER SECTION-->
      <footer class="container-fluid py-5">
          <div class="row">
              <div class="col-sm-4">
@@ -41,3 +45,23 @@
              </div>
          </div>
      </footer>
+
+     
+   
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+-->
+<?php wp_footer();?>
+</body>
+
+</html>
