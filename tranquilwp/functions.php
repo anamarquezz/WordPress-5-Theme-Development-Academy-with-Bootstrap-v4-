6d206b2a1ec355dfc0f13c0a9300e0e76a855221
anamarquez <anamarquez.edu@hotmail.com>
2021-09-20 17:01:30 -0700

    update

--- tranquilwp/functions.php
+++ tranquilwp/functions.php
@@ -12,7 +12,30 @@ if(!isset($content_width)){
 function tranquilwp_setup() {
     add_theme_support('automatic-feed-links');
     add_theme_support('title-tag');
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+
+    register_nav_menus( array(
+        'primary' => __( 'Main header menu', 'tranquilqp' ),
+    ) );
 }
 
 add_action('after_setup_theme','tranquilwp_setup');
+
+function tranquil_scripts() {
+    /*
+    Add styles
+    */
+
+    wp_enqueue_style('bootstrap-core', get_template_directory_uri() .'/css/bootstrap.min.css');
+    wp_enqueue_style('custom', get_template_directory_uri() .'/css/custom.css');
+
+    //Script
+    wp_enqueue_script('boostrap-js', get_template_directory_uri() .'/js/bootstrap.min.js', array('jquery'), null, true);
+    wp_enqueue_script('fontawesome', get_template_directory_uri() .'/js/fontawesome.js');
+    
+}
+
+add_action('wp_enqueue_scripts','tranquil_scripts');
+
+
 ?>
--- tranquilwp/header.php
+++ tranquilwp/header.php
@@ -13,20 +13,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--link href="css/bootstrap.min.css" rel="stylesheet"-->
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d276a35b25.js" crossorigin="anonymous"></script>
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/custom.css">
+    <!--link rel="stylesheet" href="css/custom.css"-->
     <?php wp_head() ?>
 </head>
 
 <body <?php body_class(); ?>>
     <header>
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'primary',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+    </div>
+</nav>
+
+        <!--nav class="navbar navbar-expand-md navbar-light bg-light">
             <a class="navbar-brand" href="<?php echo esc_url(home_url()) ?>"><?php echo get_bloginfo('name') ?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
@@ -35,9 +57,10 @@
                 <ul class="navbar-nav">
                     <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
                     <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                </ul>
+                </u>
+              
             </div>
-        </nav>
+        </nav-->
 
         <section class="container-fluid">
             <div class="row position-relative d-flex justify-content-center align-items-center text-center">
